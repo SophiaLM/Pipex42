c0c4e3fa989aa7a3ef42f413f52db78da9d3fd23
Sophia Luna <soluna@c2r12s4.42madrid.com>
2025-01-08 23:39:19 +0100

    norma correction

--- pipex.h
+++ pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soluna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 23:37:20 by soluna            #+#    #+#             */
+/*   Updated: 2025/01/08 23:38:55 by soluna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
@@ -9,19 +21,11 @@
 # include <stdlib.h>
 # include "./libft42/libft.h"
 
-
-// INICIALIZAN LOS HIJOS
 void	*first_child(char **av, int *fd, char **env);
 void	*second_child(char **av, int *fd, char **env);
-
-char    *ft_error(char *error);
-
-// ENCUENTRA EL PATH DENTRO DE ENV
+char	*ft_error(char *mensaje);
 char	*find_path(char **env);
-
-// ENCUENTRA EL COMANDO DENTRO DEL PATH
 char	*find_cmd(char *path, char *cmd);
-
 char	**relative_path(char *av, char **env);
 
 #endif
